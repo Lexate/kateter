7476c978c76969f0aff0d8d790194a1b5eb5dc4e
Lexate <47445605+Lexate@users.noreply.github.com>
2024-11-24 21:44:01 +0100

    Generate point graph

--- presentation_graph.py
+++ presentation_graph.py
@@ -7,33 +7,34 @@
 # ///
 import matplotlib.pyplot as plt
 import numpy as np
-
-# Fixing random state for reproducibility
-np.random.seed(19680801)
-
-
-def randrange(n, vmin, vmax):
-    """
-    Helper function to make an array of random numbers having shape (n, )
-    with each number distributed Uniform(vmin, vmax).
-    """
-    return (vmax - vmin)*np.random.rand(n) + vmin
+import csv
 
 fig = plt.figure()
 ax = fig.add_subplot(projection='3d')
 
 n = 100
 
-# For each set of style and range settings, plot n random points in the box
-# defined by x in [23, 32], y in [0, 100], z in [zlow, zhigh].
-for m, zlow, zhigh in [('o', -50, -25), ('^', -30, -5)]:
-    xs = randrange(n, 23, 32)
-    ys = randrange(n, 0, 100)
-    zs = randrange(n, zlow, zhigh)
-    ax.scatter(xs, ys, zs, marker=m)
+ax.set_xlabel('r1')
+ax.set_ylabel('r2')
+ax.set_zlabel('r3')
 
-ax.set_xlabel('X Label')
-ax.set_ylabel('Y Label')
-ax.set_zlabel('Z Label')
+with open("Calibration/cal.csv", mode='r', newline='') as csvfile:
+    reader = csv.reader(csvfile, delimiter = ',')
 
-plt.savefig("output.jpg")
+    colour = (0, 0, 0)
+
+    for row in reader:
+        match row[1]:
+            case '0':
+                colour = [(114/255, 135/255, 253/255)] # lavender
+            case '15':
+                colour = [(64/255, 160/255, 43/255)] # green
+            case '30':
+                colour = [(230/255, 69/255, 83/255)] # maroon
+            case '45':
+                colour = [(136/255, 57/255, 239/255)] # mauve
+
+        print(f"{float(row[2])}, {float(row[3])}, {float(row[4])}")
+        ax.scatter(float(row[2]), float(row[3]), float(row[4]), marker='o', c=colour)
+
+fig.savefig("output.jpg", dpi=300)
