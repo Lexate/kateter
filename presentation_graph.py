cc44aadc4e9c943e06dd830d778644a0c72a0589
Lexate <47445605+Lexate@users.noreply.github.com>
2024-11-28 09:06:22 +0100

    Small improvements to plot function

--- presentation_graph.py
+++ presentation_graph.py
@@ -14,27 +14,29 @@ ax = fig.add_subplot(projection='3d')
 
 n = 100
 
+ax.set_title("Kalibrerings punkter")
+
 ax.set_xlabel('r1')
 ax.set_ylabel('r2')
 ax.set_zlabel('r3')
 
-with open("Calibration/cal.csv", mode='r', newline='') as csvfile:
+with open("Calibration/cal3.csv.norm", mode='r', newline='') as csvfile:
     reader = csv.reader(csvfile, delimiter = ',')
 
     colour = (0, 0, 0)
 
     for row in reader:
-        match row[0]:
-            case '0':
+        match float(row[0]):
+            case 0:
                 colour = [(114/255, 135/255, 253/255)] # lavender
-            case '15':
+            case 15:
                 colour = [(64/255, 160/255, 43/255)] # green
-            case '30':
+            case 30:
                 colour = [(230/255, 69/255, 83/255)] # maroon
-            case '45':
+            case 45:
                 colour = [(136/255, 57/255, 239/255)] # mauve
 
         print(f"{float(row[2])}, {float(row[3])}, {float(row[4])}")
-        ax.scatter(float(row[2]), float(row[3]), float(row[4]), marker='o', c=colour)
+        ax.scatter(float(row[2]), float(row[3]), float(row[4]), marker='.', c=colour)
 
 fig.savefig("output.jpg", dpi=1000)
