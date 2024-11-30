5df1de66367c2219e820b760924493b7864419a3
Lexate <47445605+Lexate@users.noreply.github.com>
2024-11-30 21:14:01 +0100

    Add some documentation

--- presentation_graph.py
+++ presentation_graph.py
@@ -6,6 +6,7 @@
 # ]
 # ///
 import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
 import numpy as np
 import csv
 
@@ -19,6 +20,12 @@ ax.set_title("Kalibrerings punkter")
 ax.set_xlabel('r1')
 ax.set_ylabel('r2')
 ax.set_zlabel('r3')
+
+zdeg = mpatches.Patch(color=(114/255, 135/255, 253/255), label='0°')
+ftdeg = mpatches.Patch(color=(64/255, 160/255, 43/255), label='15°')
+tdeg = mpatches.Patch(color=(230/255, 69/255, 83/255), label='30°')
+ffdeg = mpatches.Patch(color=(136/255, 57/255, 239/255), label='45°')
+ax.legend(handles=[zdeg, ftdeg, tdeg, ffdeg])
 
 with open("Calibration/cal3.csv.norm", mode='r', newline='') as csvfile:
     reader = csv.reader(csvfile, delimiter = ',')
@@ -37,6 +44,6 @@ with open("Calibration/cal3.csv.norm", mode='r', newline='') as csvfile:
                 colour = [(136/255, 57/255, 239/255)] # mauve
 
         print(f"{float(row[2])}, {float(row[3])}, {float(row[4])}")
-        ax.scatter(float(row[2]), float(row[3]), float(row[4]), marker='.', c=colour)
+        ax.scatter(float(row[2]), float(row[3]), float(row[4]), marker='o', c=colour)
 
 fig.savefig("output.jpg", dpi=1000)
